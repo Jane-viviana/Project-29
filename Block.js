3a5fa4e01d26d4b2344f72514694dc3198f9f3c1
Jane <73689728+Jane-viviana@users.noreply.github.com>
2021-01-02 12:40:44 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,24 +1,27 @@
 class Block{
     constructor(x,y,width,height){
         var options = {
+            //isStatic:true,
             'restitution':0.8,
-            'friction':1.0,
-            'density':1.0
+            'friction':2.0,
+            'density':2.0
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
         World.add(world,this.body);
+        
     }
     display(){
         var angle = this.body.angle;
         var pos = this.body.position;
         push();
-        translate(this.body.position.x, this.body.position.y);
+      //  translate(0,0,this.width,this.height);
+      translate(this.body.position.x,this.body.position.y);
         rotate(angle);
         rectMode(CENTER);
         fill("white")
-        rect(pos.x, pos.y, this.width, this.height);
+        rect(0,0, this.width, this.height);
         pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -4,15 +4,36 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var ground;
-var block1;
+var ground2;
+var block1,block2,block3,block4;
+var block5,block6,block7,block8;
+var block9,block10,block11;
+var polygon;
+var sling;
+
 
 function setup(){
      var canvas = createCanvas(800,500);
      engine = Engine.create()
      world = engine.world;
 
-     ground = new Ground(475,400,200,15);
-     block1 = new Block(475,400,100,100);
+     ground = new Ground(475,400,300,15);
+     block1 = new Block(475,300,50,50);
+     block2 = new Block(450,300,50,50)
+     block3 = new Block(450,300,50,50);
+     block4 = new Block(475,300,50,50);
+     
+     ground2 = new Ground(600,200,300,15);
+     block5 = new Block(600,100,50,50);
+     block6 = new Block(600,100,50,50);
+     block7 = new Block(600,100,50,50);
+     block8 = new Block(650,20,50,50);
+     block9 = new Block(650,20,50,50);
+     block10 = new Block(550,20,50,50);
+     block11 = new Block(550,20,50,50);
+
+     polygon = new Polygon(300,300,15,15);
+     sling = new SlingShot(polygon.body,{x:300,y:300});
 }
 function draw(){
     background(0);
@@ -20,4 +41,34 @@ function draw(){
 
     ground.display();
     block1.display();
+    block2.display();
+    block3.display();
+    block4.display();
+    ground2.display();
+    block5.display();
+    block6.display();
+    block7.display();
+    block8.display();
+    block9.display();
+    block10.display();
+    block11.display();
+
+    polygon.display();
+    sling.display();
+}
+function mouseDragged(){
+    Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    sling.fly();
+}
+
+function keyPressed(){
+    
+    if(keyCode === 32){
+
+    sling.attach(polygon.body);
+    }
 }
